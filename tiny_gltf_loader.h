1371b5a63172cdcfd51ece4f71ea9aa436bccfff
Syoyo Fujita <syoyo@lighttransport.com>
2016-10-14 18:50:14 +0900

    Improve error messsage a bit.

--- tiny_gltf_loader.h
+++ tiny_gltf_loader.h
@@ -346,8 +346,7 @@ class Scene {
   Asset asset;
 };
 
-enum SectionCheck
-{
+enum SectionCheck {
   NO_REQUIRE = 0x00,
   REQUIRE_SCENE = 0x01,
   REQUIRE_SCENES = 0x02,
@@ -937,14 +936,20 @@ static bool ParseNumberArrayProperty(std::vector<double> *ret, std::string *err,
   return true;
 }
 
-static bool ParseStringProperty(std::string *ret, std::string *err,
-                                const picojson::object &o,
-                                const std::string &property, bool required) {
+static bool ParseStringProperty(
+    std::string *ret, std::string *err, const picojson::object &o,
+    const std::string &property, bool required,
+    const std::string &parent_node = std::string()) {
   picojson::object::const_iterator it = o.find(property);
   if (it == o.end()) {
     if (required) {
       if (err) {
-        (*err) += "'" + property + "' property is missing.\n";
+        (*err) += "'" + property + "' property is missing";
+        if (parent_node.empty()) {
+          (*err) += ".\n";
+        } else {
+          (*err) += " in `" + parent_node + "'.\n";
+        }
       }
     }
     return false;
@@ -1480,7 +1485,8 @@ static bool ParseAccessor(Accessor *accessor, std::string *err,
 
 static bool ParsePrimitive(Primitive *primitive, std::string *err,
                            const picojson::object &o) {
-  if (!ParseStringProperty(&primitive->material, err, o, "material", true)) {
+  if (!ParseStringProperty(&primitive->material, err, o, "material", true,
+                           "mesh.primitive")) {
     return false;
   }
 
@@ -1501,10 +1507,7 @@ static bool ParsePrimitive(Primitive *primitive, std::string *err,
   primitive->indices = "";
   ParseStringProperty(&primitive->indices, err, o, "indices", false);
 
-  if (!ParseStringMapProperty(&primitive->attributes, err, o, "attributes",
-                              true)) {
-    return false;
-  }
+  ParseStringMapProperty(&primitive->attributes, err, o, "attributes", false);
 
   return true;
 }
@@ -1671,7 +1674,7 @@ static bool ParseShader(Shader *shader, std::string *err,
     }
   } else {
     // Load shader source from data uri
-    // TODO: Support ascii or utf-8 data uris.
+    // TODO(syoyo): Support ascii or utf-8 data uris.
     if (IsDataURI(uri)) {
       if (!DecodeDataURI(&shader->source, uri, 0, false)) {
         if (err) {
@@ -2327,7 +2330,6 @@ bool TinyGLTFLoader::LoadASCIIFromFile(Scene *scene, std::string *err,
   bool ret = LoadASCIIFromString(scene, err, &buf.at(0),
                                  static_cast<unsigned int>(buf.size()), basedir,
                                  check_sections);
-
 
   return ret;
 }
