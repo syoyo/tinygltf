088aa0210f52e62fa2facff6d2d30dec2fed8121
Arthur Brainville <ybalrid@ybalrid.info>
2018-02-19 01:27:42 +0100

    Fix where stb image is actually implemented
    Signed-off-by: Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>

--- examples/raytrace/gltf-loader.cc
+++ examples/raytrace/gltf-loader.cc
@@ -3,7 +3,6 @@
 #include <iostream>
 
 #define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
 #include "tiny_gltf.h"
 
 namespace example {
@@ -17,9 +16,10 @@ static std::string GetFilePathExtension(const std::string &FileName) {
 ///
 /// Loads glTF 2.0 mesh
 ///
-bool LoadGLTF(const std::string &filename, float scale, std::vector<Mesh<float> > *meshes, std::vector<Material> *materials, std::vector<Texture> *textures) 
-{
-
+bool LoadGLTF(const std::string &filename, float scale,
+              std::vector<Mesh<float> > *meshes,
+              std::vector<Material> *materials,
+              std::vector<Texture> *textures) {
   // TODO(syoyo): Implement
   // TODO(syoyo): Texture
   // TODO(syoyo): Material
@@ -50,4 +50,4 @@ bool LoadGLTF(const std::string &filename, float scale, std::vector<Mesh<float> 
   return false;
 }
 
-} // namespace example
+}  // namespace example
