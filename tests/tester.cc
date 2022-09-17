651449009007580e5e15e50093360cfe9b3e0be3
Kh4n <asadullahmkhan@gmail.com>
2022-09-17 12:29:58 -0500

    update gitignore to remove test file
    readd accidental removals in gitignore
    
    undo autoformat
    
    more undo autoformatting

--- tests/tester.cc
+++ tests/tester.cc
@@ -6,37 +6,39 @@
 // Nlohmann json(include ../json.hpp)
 #include "json.hpp"
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
-                           // this in one cpp file
-#include <cassert>
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-static JsonDocument JsonConstruct(const char* str) {
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+static JsonDocument JsonConstruct(const char* str)
+{
   JsonDocument doc;
   JsonParse(doc, str, strlen(str));
   return doc;
 }
 
+
 TEST_CASE("parse-error", "[parse]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
   std::string warn;
 
-  bool ret = ctx.LoadASCIIFromString(&model, &err, &warn, "bora",
-                                     static_cast<int>(strlen("bora")),
-                                     /* basedir*/ "");
+  bool ret = ctx.LoadASCIIFromString(&model, &err, &warn, "bora", static_cast<int>(strlen("bora")), /* basedir*/ "");
 
   REQUIRE(false == ret);
+
 }
 
 TEST_CASE("datauri-in-glb", "[issue-79]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
@@ -51,13 +53,13 @@ TEST_CASE("datauri-in-glb", "[issue-79]") {
 }
 
 TEST_CASE("extension-with-empty-object", "[issue-97]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
   std::string warn;
 
-  bool ret = ctx.LoadASCIIFromFile(&model, &err, &warn,
-                                   "../models/Extensions-issue97/test.gltf");
+  bool ret = ctx.LoadASCIIFromFile(&model, &err, &warn, "../models/Extensions-issue97/test.gltf");
   if (!err.empty()) {
     std::cerr << err << std::endl;
   }
@@ -91,17 +93,17 @@ TEST_CASE("extension-with-empty-object", "[issue-97]") {
     REQUIRE(m.materials[0].extensions.size() == 1);
     REQUIRE(m.materials[0].extensions.count("VENDOR_material_some_ext") == 1);
   }
+
 }
 
 TEST_CASE("extension-overwrite", "[issue-261]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
   std::string warn;
 
-  bool ret = ctx.LoadASCIIFromFile(
-      &model, &err, &warn,
-      "../models/Extensions-overwrite-issue261/issue-261.gltf");
+  bool ret = ctx.LoadASCIIFromFile(&model, &err, &warn, "../models/Extensions-overwrite-issue261/issue-261.gltf");
   if (!err.empty()) {
     std::cerr << err << std::endl;
   }
@@ -110,12 +112,9 @@ TEST_CASE("extension-overwrite", "[issue-261]") {
   REQUIRE(model.extensionsUsed.size() == 3);
   {
     bool has_ext_lights = false;
-    has_ext_lights |=
-        (model.extensionsUsed[0].compare("KHR_lights_punctual") == 0);
-    has_ext_lights |=
-        (model.extensionsUsed[1].compare("KHR_lights_punctual") == 0);
-    has_ext_lights |=
-        (model.extensionsUsed[2].compare("KHR_lights_punctual") == 0);
+    has_ext_lights |= (model.extensionsUsed[0].compare("KHR_lights_punctual") == 0);
+    has_ext_lights |= (model.extensionsUsed[1].compare("KHR_lights_punctual") == 0);
+    has_ext_lights |= (model.extensionsUsed[2].compare("KHR_lights_punctual") == 0);
 
     REQUIRE(true == has_ext_lights);
   }
@@ -145,7 +144,9 @@ TEST_CASE("extension-overwrite", "[issue-261]") {
     REQUIRE(m.extensions.size() == 2);
     REQUIRE(m.extensions.count("NV_MDL"));
     REQUIRE(m.extensions.count("KHR_lights_punctual"));
+
   }
+
 }
 
 TEST_CASE("invalid-primitive-indices", "[bounds-checking]") {
@@ -200,13 +201,10 @@ TEST_CASE("glb-invalid-length", "[bounds-checking]") {
 
   // This glb has a much longer length than the provided data and should fail
   // initial range checks.
-  const unsigned char glb_invalid_length[] =
-      "glTF"
-      "\x20\x00\x00\x00"
-      "\x6c\x66\x00\x00"  //
-                          //  |     version     |     length      |
-      "\x02\x00\x00\x00"
-      "\x4a\x53\x4f\x4e{}";  //
+  const unsigned char glb_invalid_length[] = "glTF"
+      "\x20\x00\x00\x00" "\x6c\x66\x00\x00"     //
+  //  |     version     |     length      |
+      "\x02\x00\x00\x00" "\x4a\x53\x4f\x4e{}";  //
   //  |  model length   |   model format  |
 
   bool ret = ctx.LoadBinaryFromMemory(&model, &err, &warn, glb_invalid_length,
@@ -233,13 +231,13 @@ TEST_CASE("parse-integer", "[bounds-checking]") {
   SECTION("parses valid numbers") {
     std::string err;
     int result = 123;
-    CHECK(tinygltf::ParseIntegerProperty(
-        &result, &err, JsonConstruct("{\"zero\" : 0}"), "zero", true));
+    CHECK(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct("{\"zero\" : 0}"), "zero",
+                                         true));
     REQUIRE(err == "");
     REQUIRE(result == 0);
 
-    CHECK(tinygltf::ParseIntegerProperty(
-        &result, &err, JsonConstruct("{\"int\": -1234}"), "int", true));
+    CHECK(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct("{\"int\": -1234}"), "int",
+                                         true));
     REQUIRE(err == "");
     REQUIRE(result == -1234);
   }
@@ -247,8 +245,7 @@ TEST_CASE("parse-integer", "[bounds-checking]") {
   SECTION("detects missing properties") {
     std::string err;
     int result = -1;
-    CHECK_FALSE(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct(""),
-                                               "int", true));
+    CHECK_FALSE(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct(""), "int", true));
     REQUIRE_THAT(err, Catch::Contains("'int' property is missing"));
     REQUIRE(result == -1);
   }
@@ -256,8 +253,8 @@ TEST_CASE("parse-integer", "[bounds-checking]") {
   SECTION("handled missing but not required properties") {
     std::string err;
     int result = -1;
-    CHECK_FALSE(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct(""),
-                                               "int", false));
+    CHECK_FALSE(
+        tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct(""), "int", false));
     REQUIRE(err == "");
     REQUIRE(result == -1);
   }
@@ -266,14 +263,14 @@ TEST_CASE("parse-integer", "[bounds-checking]") {
     std::string err;
     int result = -1;
 
-    CHECK_FALSE(tinygltf::ParseIntegerProperty(
-        &result, &err, JsonConstruct("{\"int\": 0.5}"), "int", true));
+    CHECK_FALSE(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct("{\"int\": 0.5}"),
+      "int", true));
     REQUIRE_THAT(err, Catch::Contains("not an integer type"));
 
     // Excessively large values and NaN aren't allowed either.
     err.clear();
-    CHECK_FALSE(tinygltf::ParseIntegerProperty(
-        &result, &err, JsonConstruct("{\"int\": 1e300}"), "int", true));
+    CHECK_FALSE(tinygltf::ParseIntegerProperty(&result, &err, JsonConstruct("{\"int\": 1e300}"),
+      "int", true));
     REQUIRE_THAT(err, Catch::Contains("not an integer type"));
 
     err.clear();
@@ -281,8 +278,9 @@ TEST_CASE("parse-integer", "[bounds-checking]") {
       JsonDocument o;
       double nan = std::numeric_limits<double>::quiet_NaN();
       tinygltf::JsonAddMember(o, "int", json(nan));
-      CHECK_FALSE(
-          tinygltf::ParseIntegerProperty(&result, &err, o, "int", true));
+      CHECK_FALSE(tinygltf::ParseIntegerProperty(
+        &result, &err, o,
+        "int", true));
       REQUIRE_THAT(err, Catch::Contains("not an integer type"));
     }
   }
@@ -306,19 +304,19 @@ TEST_CASE("parse-unsigned", "[bounds-checking]") {
     std::string err;
     size_t result = -1;
 
-    CHECK_FALSE(tinygltf::ParseUnsignedProperty(
-        &result, &err, JsonConstruct("{\"int\": -1234}"), "int", true));
+    CHECK_FALSE(tinygltf::ParseUnsignedProperty(&result, &err, JsonConstruct("{\"int\": -1234}"),
+                                                "int", true));
     REQUIRE_THAT(err, Catch::Contains("not a positive integer"));
 
     err.clear();
-    CHECK_FALSE(tinygltf::ParseUnsignedProperty(
-        &result, &err, JsonConstruct("{\"int\": 0.5}"), "int", true));
+    CHECK_FALSE(tinygltf::ParseUnsignedProperty(&result, &err, JsonConstruct("{\"int\": 0.5}"),
+                                                "int", true));
     REQUIRE_THAT(err, Catch::Contains("not a positive integer"));
 
     // Excessively large values and NaN aren't allowed either.
     err.clear();
-    CHECK_FALSE(tinygltf::ParseUnsignedProperty(
-        &result, &err, JsonConstruct("{\"int\": 1e300}"), "int", true));
+    CHECK_FALSE(tinygltf::ParseUnsignedProperty(&result, &err, JsonConstruct("{\"int\": 1e300}"),
+                                                "int", true));
     REQUIRE_THAT(err, Catch::Contains("not a positive integer"));
 
     err.clear();
@@ -326,8 +324,9 @@ TEST_CASE("parse-unsigned", "[bounds-checking]") {
       JsonDocument o;
       double nan = std::numeric_limits<double>::quiet_NaN();
       tinygltf::JsonAddMember(o, "int", json(nan));
-      CHECK_FALSE(
-          tinygltf::ParseUnsignedProperty(&result, &err, o, "int", true));
+      CHECK_FALSE(tinygltf::ParseUnsignedProperty(
+        &result, &err, o,
+        "int", true));
       REQUIRE_THAT(err, Catch::Contains("not a positive integer"));
     }
   }
@@ -337,8 +336,8 @@ TEST_CASE("parse-integer-array", "[bounds-checking]") {
   SECTION("parses valid integers") {
     std::string err;
     std::vector<int> result;
-    CHECK(tinygltf::ParseIntegerArrayProperty(
-        &result, &err, JsonConstruct("{\"x\": [-1, 2, 3]}"), "x", true));
+    CHECK(tinygltf::ParseIntegerArrayProperty(&result, &err,
+                                              JsonConstruct("{\"x\": [-1, 2, 3]}"), "x", true));
     REQUIRE(err == "");
     REQUIRE(result.size() == 3);
     REQUIRE(result[0] == -1);
@@ -363,27 +362,22 @@ TEST_CASE("pbr-khr-texture-transform", "[material]") {
 
   // Loading is expected to fail, but not crash.
   bool ret = ctx.LoadASCIIFromFile(
-      &model, &err, &warn, "../models/Cube-texture-ext/Cube-textransform.gltf");
+      &model, &err, &warn,
+      "../models/Cube-texture-ext/Cube-textransform.gltf");
   REQUIRE(ret == true);
 
   REQUIRE(model.materials.size() == 2);
-  REQUIRE(model.materials[0].emissiveTexture.extensions.count(
-              "KHR_texture_transform") == 1);
-  REQUIRE(model.materials[0]
-              .emissiveTexture.extensions["KHR_texture_transform"]
-              .IsObject());
+  REQUIRE(model.materials[0].emissiveTexture.extensions.count("KHR_texture_transform") == 1);
+  REQUIRE(model.materials[0].emissiveTexture.extensions["KHR_texture_transform"].IsObject());
 
-  tinygltf::Value::Object& texform =
-      model.materials[0]
-          .emissiveTexture.extensions["KHR_texture_transform"]
-          .Get<tinygltf::Value::Object>();
+  tinygltf::Value::Object &texform = model.materials[0].emissiveTexture.extensions["KHR_texture_transform"].Get<tinygltf::Value::Object>();
 
   REQUIRE(texform.count("scale"));
 
   REQUIRE(texform["scale"].IsArray());
 
-  // Note: It looks json.hpp parse integer JSON number as integer, not floating
-  // point. IsNumber return true either value is int or floating point.
+  // Note: It looks json.hpp parse integer JSON number as integer, not floating point.
+  // IsNumber return true either value is int or floating point.
   REQUIRE(texform["scale"].Get(0).IsNumber());
   REQUIRE(texform["scale"].Get(1).IsNumber());
 
@@ -393,18 +387,18 @@ TEST_CASE("pbr-khr-texture-transform", "[material]") {
 
   REQUIRE(scale[0] == Approx(1.0));
   REQUIRE(scale[1] == Approx(-1.0));
+
 }
 
 TEST_CASE("image-uri-spaces", "[issue-236]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
   std::string warn;
 
   // Test image file with single spaces.
-  bool ret = ctx.LoadASCIIFromFile(
-      &model, &err, &warn,
-      "../models/CubeImageUriSpaces/CubeImageUriSpaces.gltf");
+  bool ret = ctx.LoadASCIIFromFile(&model, &err, &warn, "../models/CubeImageUriSpaces/CubeImageUriSpaces.gltf");
   if (!err.empty()) {
     std::cerr << err << std::endl;
   }
@@ -413,9 +407,7 @@ TEST_CASE("image-uri-spaces", "[issue-236]") {
 
   // Test image file with a beginning space, trailing space, and greater than
   // one consecutive spaces.
-  ret = ctx.LoadASCIIFromFile(
-      &model, &err, &warn,
-      "../models/CubeImageUriSpaces/CubeImageUriMultipleSpaces.gltf");
+  ret = ctx.LoadASCIIFromFile(&model, &err, &warn, "../models/CubeImageUriSpaces/CubeImageUriMultipleSpaces.gltf");
   if (!err.empty()) {
     std::cerr << err << std::endl;
   }
@@ -424,11 +416,11 @@ TEST_CASE("image-uri-spaces", "[issue-236]") {
 }
 
 TEST_CASE("serialize-empty-material", "[issue-294]") {
+
   tinygltf::Model m;
 
   tinygltf::Material mat;
-  mat.pbrMetallicRoughness.baseColorFactor = {1.0f, 1.0f, 1.0f,
-                                              1.0f};  // default baseColorFactor
+  mat.pbrMetallicRoughness.baseColorFactor = {1.0f, 1.0f, 1.0f, 1.0f}; // default baseColorFactor
   m.materials.push_back(mat);
 
   std::stringstream os;
@@ -441,23 +433,24 @@ TEST_CASE("serialize-empty-material", "[issue-294]") {
 
   REQUIRE(1 == j["materials"].size());
   REQUIRE(j["materials"][0].is_object());
+
 }
 
 TEST_CASE("empty-skeleton-id", "[issue-321]") {
+
   tinygltf::Model model;
   tinygltf::TinyGLTF ctx;
   std::string err;
   std::string warn;
 
-  bool ret = ctx.LoadASCIIFromFile(
-      &model, &err, &warn, "../models/regression/unassigned-skeleton.gltf");
+  bool ret = ctx.LoadASCIIFromFile(&model, &err, &warn, "../models/regression/unassigned-skeleton.gltf");
   if (!err.empty()) {
     std::cerr << err << std::endl;
   }
   REQUIRE(true == ret);
 
   REQUIRE(model.skins.size() == 1);
-  REQUIRE(model.skins[0].skeleton == -1);  // unassigned
+  REQUIRE(model.skins[0].skeleton == -1); // unassigned
 
   std::stringstream os;
 
@@ -470,7 +463,25 @@ TEST_CASE("empty-skeleton-id", "[issue-321]") {
   REQUIRE(1 == j["skins"].size());
   REQUIRE(j["skins"][0].is_object());
   REQUIRE(j["skins"][0].count("skeleton") == 0);
+
 }
+
+#ifndef TINYGLTF_NO_FS
+TEST_CASE("expandpath-utf-8", "[pr-226]") {
+
+  std::string s1 = "\xe5\xaf\xb9"; // utf-8 string
+
+  std::string ret = tinygltf::ExpandFilePath(s1, /* userdata */nullptr);
+
+  // expected: E5 AF B9
+  REQUIRE(3 == ret.size());
+
+  REQUIRE(0xe5 == static_cast<uint8_t>(ret[0]));
+  REQUIRE(0xaf == static_cast<uint8_t>(ret[1]));
+  REQUIRE(0xb9 == static_cast<uint8_t>(ret[2]));
+
+}
+#endif
 
 TEST_CASE("empty-bin-buffer", "[issue-382]") {
   tinygltf::Model model;
@@ -519,42 +530,4 @@ TEST_CASE("empty-bin-buffer", "[issue-382]") {
     std::cerr << "there should have been an error reported" << std::endl;
   }
   REQUIRE(false == ret);
-
-  // bool ret = ctx.LoadBinaryFromFile(
-  //     &model, &err, &warn, "../models/EmptyBinChunk-issue382/empty.glb");
-  // if (!err.empty()) {
-  //   std::cerr << err << std::endl;
-  // }
-  // REQUIRE(true == ret);
-
-  // ret = ctx.LoadBinaryFromFile(
-  //     &model, &err, &warn,
-  //     "../models/EmptyBinChunk-issue382/empty_buffer.glb");
-  // if (err.empty()) {
-  //   std::cerr << "there should have been an error reported" << std::endl;
-  // }
-  // REQUIRE(false == ret);
-
-  // ret = ctx.LoadBinaryFromFile(
-  //     &model, &err, &warn,
-  //     "../models/EmptyBinChunk-issue382/single_byte_buffer.glb");
-  // if (err.empty()) {
-  //   std::cerr << "there should have been an error reported" << std::endl;
-  // }
-  // REQUIRE(false == ret);
 }
-
-#ifndef TINYGLTF_NO_FS
-TEST_CASE("expandpath-utf-8", "[pr-226]") {
-  std::string s1 = "\xe5\xaf\xb9";  // utf-8 string
-
-  std::string ret = tinygltf::ExpandFilePath(s1, /* userdata */ nullptr);
-
-  // expected: E5 AF B9
-  REQUIRE(3 == ret.size());
-
-  REQUIRE(0xe5 == static_cast<uint8_t>(ret[0]));
-  REQUIRE(0xaf == static_cast<uint8_t>(ret[1]));
-  REQUIRE(0xb9 == static_cast<uint8_t>(ret[2]));
-}
-#endif
